591b671e87cf71fc1381c9ac37bc1a5c6c532786
llirik42 <kondrenkokp@yandex.ru>
2022-11-05 13:04:14 +0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <any>
+
+int main(){
+
+    // string_view
+    //    std::byte t = std::;
+    std::any a = 42;
+    a = 11.34f;
+    a = std::string{ "hello" };
+    return 0;
+}
