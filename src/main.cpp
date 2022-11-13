d8c1f74fc30676cf26e6c02a0e4b466e40e1d704
llirik42 <kondrenkokp@yandex.ru>
2022-11-13 15:32:30 +0700

    Finished args parsing ))

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,44 @@
-#include <iostream>
+#include <argparse/argparse.hpp>
 
-int main(){
+int main(int argc, char *argv[]){
+    argparse::ArgumentParser program("SoundProcessor");
+    program.add_description("The program performs audio conversions specified in the config "
+                            "file and saves result to an output file.\n"
+                            "Supports only WAV files with: "
+                            "audio-format - PCM (without encoding), "
+                            "1 channel (mono), "
+                            "bit depth - signed 16 bit (little endian), "
+                            "frequency - 44100 Hz");
+
+    program.add_argument("output_file")
+            .help("The output file")
+            .required();
+
+    program.add_argument("input_file")
+            .help("The input file")
+            .required();
+
+    program.add_argument("auxiliary_files")
+            .help("auxiliary_files")
+            .nargs(argparse::nargs_pattern::any);
+
+    program.add_argument("-c", "--config")
+            .help("Specify config file")
+            .required();
+
+    try {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        return 1;
+    }
+
+    auto config_file = program.get<std::string>("-c");
+    auto output_file = program.get<std::string>("output_file");
+    auto input_file = program.get<std::string>("input_file");
+    auto auxiliary_files = program.get<std::vector<std::string>>("auxiliary_files");
+
     return 0;
 }
