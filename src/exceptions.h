eb92cd9b834fa835086b5bf9a9eff335a36c867c
llirik42 <kondrenkokp@yandex.ru>
2022-11-26 14:39:28 +0700

    Minor fixes for prev commit

--- src/exceptions.h
+++ src/exceptions.h
@@ -2,16 +2,17 @@
 
 #include <exception>
 
+const unsigned int INCORRECT_WAV_ERROR_CODE = 1;
+
 class AbstractException : public std::exception{
 public:
-    [[nodiscard]] const char* what() const noexcept override=0;
-    [[nodiscard]] virtual int code() const noexcept=0;
-    ~AbstractException()=default;
+    [[nodiscard]] const char* what() const noexcept override = 0;
+    [[nodiscard]] virtual int code() const noexcept = 0;
 private:
 };
 
 class IncorrectWavError : public AbstractException{
 public:
     [[nodiscard]] const char* what() const noexcept override {return "Incorrect .wav file";}
-    [[nodiscard]] int code() const noexcept override {return 1;}
+    [[nodiscard]] int code() const noexcept override {return INCORRECT_WAV_ERROR_CODE;}
 };
